fe45d90fffebad5464c688cb8adc0bf4e5e05136
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-05-17 15:51:06 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,11 @@ class PostsController < ApplicationController
   def edit; end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
 private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Author < ActiveRecord::Base
+  validates :name, length: { minimum: 2 }
+  validates :email, uniqueness: true
 end
